fb21951068b72639fc821b6ac65794b7a680706f
svk31 <sigvekvalsvik@gmail.com>
2015-06-29 10:50:44 +0200

    Attempt to include Highstock instead of Highcharts

---
+++ src/Highstock.jsx
@@ -0,0 +1,36 @@
+global.HighchartsAdapter = require('exports?HighchartsAdapter!Highcharts/js/adapters/standalone-framework.src');
+var Highcharts = require('exports?Highcharts!Highcharts/js/highstock.src');
+var React = require('react');
+var update = require('react/addons').addons.update;
+module.exports = React.createClass({
+  displayName: 'Highstock',
+
+  renderChart: function () {
+    if (!this.props.config) {
+      throw new Error('Config has to be specified, for the Highchart component');
+    }
+
+    var config = this.props.config;
+    var node = this.refs.chart.getDOMNode();
+
+    if (!config.chart) {
+      config = update(config, {chart: {$set: {}}})
+    }
+
+    config = update(config, {chart: {renderTo: {$set: node}}});
+
+    new Highcharts.Chart(config);
+  },
+
+  componentDidMount: function () {
+    this.renderChart();
+  },
+  componentDidUpdate: function () {
+    this.renderChart();
+  },
+  render: function () {
+    return <div className = "chart" ref = "chart" />
+  }
+});
+
+module.exports.Highstock = Highstock;
